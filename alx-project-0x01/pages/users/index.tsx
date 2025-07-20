78054b14fb1bf365a5e9a184c7691d33368dc146
Zoe <zoe@berthengineering.com>
2025-07-20 22:44:19 +0000

    Updated users

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,20 +1,46 @@
 import Header from "@/components/layout/Header";
 import UserCard from "@/components/common/UserCard";
-import { UserProps } from "@/interfaces";
+import UserModal from "@/components/common/UserModal";
+import { UserProps, UserData } from "@/interfaces";
+import { useState } from "react";
 
 interface UsersPageProps {
   posts: UserProps[];
 }
 
 const Users: React.FC<UsersPageProps> = ({ posts }) => {
+  const [users, setUsers] = useState<UserProps[]>(posts);
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleAddUser = (newUser: UserData) => {
+    setUsers((prev) => [...prev, newUser]);
+  };
+
   return (
     <div className="min-h-screen bg-gray-100">
       <Header />
-      <div className="max-w-5xl mx-auto p-4 grid gap-6">
-        {posts.map((user) => (
-          <UserCard key={user.id} {...user} />
-        ))}
+      <div className="max-w-5xl mx-auto p-4">
+        <div className="flex justify-end mb-4">
+          <button
+            onClick={() => setModalOpen(true)}
+            className="px-4 py-2 bg-blue-600 text-white rounded"
+          >
+            Add User
+          </button>
+        </div>
+
+        <div className="grid gap-6">
+          {users.map((user) => (
+            <UserCard key={user.id} {...user} />
+          ))}
+        </div>
       </div>
+
+      <UserModal
+        isOpen={isModalOpen}
+        onClose={() => setModalOpen(false)}
+        onSubmit={handleAddUser}
+      />
     </div>
   );
 };
